755d8b309194c08ffb359f529532659d90ee0cd9
Eduardo Gurgel <eduardo@gurgel.me>
2016-12-27 09:40:32 +1300

    Change lpop_rpush_src_dest to work in batches
    It will fetch a maximum of 1000 at a time when enqueue jobs from
    the inprogress list

--- priv/lpop_rpush_src_dest.lua
+++ priv/lpop_rpush_src_dest.lua
@@ -1,10 +1,11 @@
-local jobs = redis.call('LRANGE', KEYS[1], 0, -1)
+local size = tonumber(ARGV[1])
+local jobs = redis.call('LRANGE', KEYS[1], 0, size-1)
 
 if table.getn(jobs) > 0 then
   local queue_key = KEYS[2]
-  redis.call("RPUSH", queue_key, unpack(jobs))
+  redis.call('RPUSH', queue_key, unpack(jobs))
 
-  redis.call('DEL', KEYS[1])
+  redis.call('LTRIM', KEYS[1], size, -1)
 end
 
-return table.getn(jobs)
+return redis.call('LLEN', KEYS[1])
